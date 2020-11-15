8ca72642ea0e21709ddc915e6913c1bac1112a18
JuNyangYee <46080154+JuNyangYee@users.noreply.github.com>
2020-11-15 22:19:12 +0900

    pixel color update

--- js/sketch.js
+++ js/sketch.js
@@ -28,20 +28,59 @@ function setup() {
   centerCanvas();
   cn.parent('p5');
 
+  colorMode(HSB);
+
   cols = floor(width / scl);
   rows = floor(height / scl);
 
   colors = make2Darray(cols, rows);
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      colors[x][y] = color(random(255), 125, 255);
+    }
+  }
+  frameRate(1);
 }
 
 function draw() {
-  //background('#151515');
+  background('#151515');
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      colors[x][y] = random(255);
       fill(colors[x][y]);
+      stroke(180);
       rect(x * scl, y * scl, scl, scl);
+      colors[x][y] = colorChange(x, y);
     }
   }
 }
 
+function colorChange(x, y) {
+  let colorTop, colorBottom, colorLeft, colorRight;
+  let returnColor, colorSet = 0;
+  let temp;
+
+  if (y < rows-1) {
+    colorTop = hue(colors[x][y + 1]);
+    colorSet += colorTop;
+  }
+  if (y > 0) {
+    colorBottom = hue(colors[x][y - 1]);
+    colorSet += colorBottom;
+  }
+  if (x > 0) {
+    colorLeft = hue(colors[x - 1][y]);
+    colorSet += colorLeft;
+  }
+  if (x < cols-1) {
+    colorRight = hue(colors[x + 1][y]);
+    colorSet += colorRight;
+  }
+
+  colorSet *= noise(frameCount);
+  console.log(colorSet);
+
+  returnColor = color(colorSet, 100, 255);
+
+  return returnColor;
+}
