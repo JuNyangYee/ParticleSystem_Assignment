b291d5d1185a88ea746762207ee8f02a30f03229
JuNyangYee <46080154+JuNyangYee@users.noreply.github.com>
2020-11-16 18:52:26 +0900

    oop_first_update (error-occurred)

--- js/sketch.js
+++ js/sketch.js
@@ -4,7 +4,7 @@ let inc = 0.1;
 let scl = 10;
 let cols, rows;
 
-let colors = [];
+let block = [];
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -29,58 +29,78 @@ function setup() {
   cn.parent('p5');
 
   colorMode(HSB);
+  frameRate(10);
 
   cols = floor(width / scl);
   rows = floor(height / scl);
 
-  colors = make2Darray(cols, rows);
+  block = make2Darray(cols, rows);
 
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      colors[x][y] = color(random(255), 125, 255);
+      block[x][y] = new Block();
     }
   }
-  frameRate(1);
 }
 
 function draw() {
   background('#151515');
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      fill(colors[x][y]);
-      stroke(180);
-      rect(x * scl, y * scl, scl, scl);
-      colors[x][y] = colorChange(x, y);
+      block[x][y].update(x, y);
+      block[x][y].display(x, y);
     }
   }
 }
 
-function colorChange(x, y) {
-  let colorTop, colorBottom, colorLeft, colorRight;
-  let returnColor, colorSet = 0;
-  let temp;
-
-  if (y < rows-1) {
-    colorTop = hue(colors[x][y + 1]);
-    colorSet += colorTop;
-  }
-  if (y > 0) {
-    colorBottom = hue(colors[x][y - 1]);
-    colorSet += colorBottom;
-  }
-  if (x > 0) {
-    colorLeft = hue(colors[x - 1][y]);
-    colorSet += colorLeft;
-  }
-  if (x < cols-1) {
-    colorRight = hue(colors[x + 1][y]);
-    colorSet += colorRight;
+class Block {
+  construct() {
+    this.fillColor = color(random(255), 125, 255);
   }
 
-  colorSet *= noise(frameCount);
-  console.log(colorSet);
+  update(x, y, block) {
+    this.fillColor = this.colorChange(x, y);
+  }
 
-  returnColor = color(colorSet, 100, 255);
+  display(x, y) {
+    fill(this.fillColor);
+    stroke(180);
+    rect(x * scl, y * scl, scl, scl);
+  }
+  
+  colorChange(x, y,) {
+    // let colorTop, colorBottom, colorLeft, colorRight;
+    // let returnColor, colorSet = 0;
 
-  return returnColor;
+    this.colorSet = 0;
+
+    if (y < rows-1) {
+      this.c = get(x+3, y+scl+3);
+      this.colorTop = hue(this.c);
+      this.colorSet += this.colorTop;
+    }
+    if (y > 0) {
+      this.c = get(x+3, y-3);
+      this.colorBottom = hue(this.c);
+      this.colorSet += this.colorBottom;
+    }
+    if (x > 0) {
+      this.c = get(x-3, y+3);
+      this.colorLeft = hue(this.c);
+      this.colorSet += this.colorLeft;
+    }
+    if (x < cols-1) {
+      this.c = get(x+scl+5, y+3);
+      this.colorRight = hue(this.c);
+      this.colorSet += this.colorRight;
+    }
+
+    this.colorSet *= noise(frameCount);
+
+    console.log(this.colorSet);
+
+    this.returnColor = color(this.colorSet, 100, 255);
+
+    return this.returnColor;
+  }
 }
